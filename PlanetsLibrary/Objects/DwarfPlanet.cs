6f6539d9396834577efc41e40328d8559af57897
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-14 10:19:20 +0100

    :sparkles: Add DwarfPlanet

---
+++ PlanetsLibrary/Objects/DwarfPlanet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using PlanetsLibrary.Core;
+
+namespace PlanetsLibrary.SpaceObjects {
+    public class DwarfPlanet : Planet {
+        public DwarfPlanet(String name) : base(name) { }
+    }
+}
